5f0d0db520c8fbe2f5c76d9b76ca047af291941c
Tarmo <tarmo888@hotmail.com>
2020-10-21 04:18:08 +0300

    undefined extended

--- webserver/index.js
+++ webserver/index.js
@@ -13,7 +13,7 @@ function start(infoByPoolAsset, eligiblePoolsByAddress, poolAssetPrices){
 
 	app.set('views', path.join(__dirname, 'views'));
 	app.set('view engine', 'ejs');
-	app.use(express.urlencoded());
+	app.use(express.urlencoded({ extended: false }));
 
 	app.get('/', async (req, res) => {
 		if (validationUtils.isValidAddress(req.query.address)){
