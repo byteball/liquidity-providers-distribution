5e131e65819d5db085b190026466aeb36ea7fdca
tony <tonyofbyteball@users.noreply.github.com>
2022-12-27 14:07:11 +0100

    0 ts

--- webserver/index.js
+++ webserver/index.js
@@ -146,7 +146,7 @@ function start(infoByPoolAsset, eligiblePoolsByAddress, poolAssetPrices){
 			const amount = assocAmountVars[key];
 			const asset = key.split('_')[2];
 			const timestamp = (assocTsVars[ "ts_" + address + "_" + asset] + conf.lock_period_in_days * 24 * 3600) || 0;
-			const to = moment().to(moment.unix(timestamp));
+			const to = timestamp ? moment().to(moment.unix(timestamp)) : 'yes';
 			const bUnlockable = moment.unix(timestamp).isBefore() && amount;
 			balances.push({amount, asset, to, bUnlockable} )
 		}
